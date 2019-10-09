2f68bec3950cb732d527d0f3e74ec9d3f020ce6a
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2019-10-09 11:59:07 -0600

    Sean

--- Project/Services/GolfService.cs
+++ Project/Services/GolfService.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using golfcard.Interfaces;
 using golfcard.Models;
 
 namespace golfcard.Services
@@ -6,6 +7,16 @@ namespace golfcard.Services
     public class GolfService
     {
         public List<Message> Messages { get; set; }
+        private IGame _game { get; set; }
 
+        public List<ICourse> Courses { get; set; }
+        public void AddHoleScores(int[] scores)
+        {
+
+        }
+        public void NewGame(int course, string[] names)
+        {
+
+        }
     }
 }
